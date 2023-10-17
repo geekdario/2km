6aea4d260c46039862c71d3d271ac24e89ef4c87
geekdario <geekdario95@gmail.com>
2023-10-17 05:13:27 +0000

    es

--- app/Components/Atouts/Atouts.tsx
+++ app/Components/Atouts/Atouts.tsx
@@ -25,18 +25,18 @@ export const Atouts = () => {
         Description:"Lorem ipsum dolor sit amet consectetur adipisicing elit"
     }]
   return (
-    <div className='md:flex justify-center  items-center w-full h-screen'>
-        <div>
-            <Image src={programmer} alt='le programmeur' className='ml-5 h-96 transition-all hover:opacity-70 hover:transition-all hover:delay-150 '/>
+    <div className='flex-wrap md:flex justify-center  items-center w-full py-5'>
+        <div className='' >
+            <Image src={programmer} alt='le programmeur' className='md:ml-5 h-72 md:h-96 transition-all hover:opacity-70 hover:transition-all hover:delay-150 '/>
         </div>
         <div className='md:flex flex-col'>
-            <h2 className='text-4xl leading-normal'>Découverte de certaines<br/><span className='bg-pblue px-2 mr-2 font-extrabold shadow-lg rounded-tl-2xl rounded-br-2xl mt-2'>Atouts</span>durant ma formation</h2>
+            <h2 className='text-2xl md:text-4xl leading-normal text-center'>Découverte de certaines<br/><span className='bg-pblue px-2 mr-2 font-extrabold shadow-lg rounded-tl-2xl rounded-br-2xl mt-2'>Atouts</span>durant ma formation</h2>
             <p className='pt-14'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> A officia molestiae dolorum tempora ut accusamus cupiditate!<br/> Nesciunt tempora reiciendis libero voluptate!</p>
-            <ul className=''>
+            <ul className=''> 
                 {Listatouts.map((List)=>(<li key={List.id} className='py-3 '>
                     
-                    <h3 className='text-2xl font-extrabold uppercase pr-6'>
-                     <span className='text-pblue text-3xl  '>0{List.id}.</span> {List.title}   
+                    <h3 className='text-xl text-center md:text-left md:text-2xl font-extrabold uppercase pr-6'>
+                     <span className='text-pblue text-2xl md:text-3xl  '>0{List.id}.</span> {List.title}   
                     </h3>
                     <p className='pl-16'>
                         {List.Description}
