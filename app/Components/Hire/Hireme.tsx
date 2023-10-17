8f233d9bb5fe3228da8b4e318236b809105c2b5d
geekdario <geekdario95@gmail.com>
2023-10-17 15:02:51 +0000

    MISEAJOURGRIDHIREMOBILE

--- app/Components/Hire/Hireme.tsx
+++ app/Components/Hire/Hireme.tsx
@@ -4,41 +4,41 @@ import {BriefcaseIcon} from "@heroicons/react/24/solid"
 import {ClockIcon} from "@heroicons/react/24/solid"
 import {MapPinIcon} from "@heroicons/react/24/solid"
 import {ComputerDesktopIcon} from "@heroicons/react/24/solid"
-import { motion } from "framer-motion"
+// import { motion } from "framer-motion"
 
 
 
 export const Hireme = () => {
   return (
-    <div className='flex-wrap md:flex justify-center items-center w-full py-14'>
+    <div className=' md:flex justify-center items-center w-full py-14 flex-wrap'>
         
     
-    <div className='md:flex flex-col '>
-        <h2 className='text-2xl md:text-4xl md:leading-normal  font-semibold'>Aviez-vous besoin<br/>de mes <span className='bg-pblue px-2 mr-2 font-extrabold shadow-lg rounded-tl-2xl rounded-br-2xl mt-2'>Services</span>?</h2>
-        <p className='pt-11 font-sans'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> A officia molestiae dolorum tempora ut accusamus cupiditate!<br/> Nesciunt tempora reiciendis libero voluptate!</p>
+    <div className='flex-wrap  md:flex flex-col md:items-start'>
+        <h2 className='text-2xl text-center md:text-4xl md:leading-normal  font-semibold'>Aviez-vous besoin<br/>de mes <span className='bg-pblue px-2 mr-2 font-extrabold shadow-lg rounded-tl-2xl rounded-br-2xl mt-2'>Services</span>?</h2>
+        <p className='pt-11 font-sans text-center md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> A officia molestiae dolorum tempora ut accusamus cupiditate!<br/> Nesciunt tempora reiciendis libero voluptate!</p>
         
-            <div className='pt-5 grid grid-cols-2 gap-3 '>
-                <div className='flex '>
+            <div className='pt-5  grid grid-col-1 sm:grid-cols-2 sm:gap-2 md:gap-3 '>
+                <div className='flex justify-center  py-5 md:justify-start'>
 
                    <BriefcaseIcon className="h-14 w-14 cursor-pointer text-pblue bg-pblue bg-opacity-10 p-2 rounded-lg hover:bg-fpblue hover:text-white transition-all hover:transition-all hover:delay-150 hover:-translate-y-1" />
                 <div className='pl-4'>
-                    <h3 className='font-semibold text-2xl'>Hire me</h3>
+                    <h3 className='font-semibold text-l md:text-2xl'>Hire me</h3>
                     <p className='font-sans'>Lorem ipsum dolor sit <br/> amet consectetur adipisicing <br/> amet consectetur adipisicing</p>
                 </div>
                 </div>
 
 
-                <div className='flex'>
+                <div className='flex justify-center  py-5 md:justify-start'>
 
                     <ClockIcon className="h-14 w-14 cursor-pointer text-pblue bg-pblue bg-opacity-10 p-2 rounded-lg hover:bg-fpblue hover:text-white transition-all hover:transition-all hover:delay-150 hover:-translate-y-1" />
                     <div className='pl-4'>
-                    <h3 className='font-semibold text-2xl'>Disponibilité</h3>
+                    <h3 className='font-semibold text-l md:text-2xl'>Disponibilité</h3>
                     <p className='font-sans'>Lorem ipsum dolor sit <br/> amet consectetur adipisicing <br/> amet consectetur adipisicing</p>
                 </div>
                 </div>
 
 
-                <div className='flex'>
+                <div className='flex justify-center  py-5 md:justify-start'>
 
                         <MapPinIcon className="h-14 w-14 cursor-pointer text-pblue bg-pblue bg-opacity-10 p-2 rounded-lg hover:bg-fpblue hover:text-white transition-all hover:transition-all hover:delay-150 hover:-translate-y-1" />
                         <div className='pl-4'>
@@ -48,7 +48,7 @@ export const Hireme = () => {
                 </div>  
 
 
-                  <div className='flex'>
+                  <div className='flex justify-center py-5 md:justify-start'>
 
                         <ComputerDesktopIcon className="h-14 w-14  cursor-pointer text-pblue bg-pblue bg-opacity-10 p-2 rounded-lg hover:bg-fpblue hover:text-white transition-all hover:transition-all hover:delay-150 hover:-translate-y-1" />
                         <div className='pl-4'>
@@ -62,9 +62,10 @@ export const Hireme = () => {
         
     </div>
 
-    <motion.div whileHover={{ scale: 1.2 }} onHoverStart={e => {}} onHoverEnd={e => {}} className='' >
-        <Image src={pgmservices} alt='le programmeur' className=' h-72 md:h-96  transition-all hover:opacity-70 hover:transition-all hover:delay-150 '/>
-    </motion.div>
+    <div className='' >
+      
+        <Image src={pgmservices} alt='le programmeur' className=' h-72 sm:52 md:h-96 animate-none  transition-all hover:opacity-70 hover:transition-all hover:delay-150'/>
+    </div>
 
     
     
