802425690224791f2237a21c515b8abac69d2e26
geekdario <geekdario95@gmail.com>
2023-10-24 16:46:08 +0000

    NAV

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+
+import React, {useState } from 'react'
 import { Bars3Icon } from '@heroicons/react/24/solid'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { DocumentArrowDownIcon} from '@heroicons/react/24/solid'
